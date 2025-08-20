274a1c2af3770c5d95d4a47384c92e66ee7ffd99
sreypheak <phorn.sreypheak@institute.pse.ngo>
2025-08-20 10:53:58 +0700

    add picture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
 import HeroSection from "@/app/home/content/hero-section";
 import Category from "@/app/home/content/category";
 import Review from "@/app/home/content/review";
-import Contact from "@/app/contact/page";
-import BlogProduct from "@/app/blog-product/page";
+
 export default function HomePage() {
   return (
     <>
       <HeroSection />
       <Category />
       <Review />
-      <Contact />
-      <BlogProduct />
     </>
   );
 }
