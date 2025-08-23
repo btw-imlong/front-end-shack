9cf98ef3f3615736adef000d30864a44066752dc
mary <thoeun.mary@institute.pse.ngo>
2025-08-23 09:43:48 +0700

    change style

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -115,7 +115,7 @@ export default function StorePage() {
       </section>
 
       {/* Stores List */}
-      <section className="max-w-4xl mx-auto px-6 py-10 text-center md:text-left">
+      {/* <section className="max-w-4xl mx-auto px-6 py-10 text-center md:text-left">
         {stores.length === 0 && <p>No store info available</p>}
         {stores.map((store) => (
           <div key={store.id} className="mb-10 border-b pb-6">
@@ -123,8 +123,35 @@ export default function StorePage() {
             {store.address && <p>📍 {store.address}</p>}
           </div>
         ))}
-      </section>
+      </section> */}
+      {/* address */}
+      <div className="bg-white text-gray-800 py-10 px-6">
+        {/* Store Info */}
+        <div className="max-w-4xl mx-auto text-center">
+          <h2 className="text-3xl font-bold mb-4">Store Location</h2>
+          <p className="text-lg">
+            📍 179-193 Preah Ang Yukanthor Street (Street 19), Daun Penh, Phnom
+            Penh
+          </p>
+          <p className="text-gray-600">
+            ⏰ Open Tuesday – Sunday: 9:00 AM – 6:00 PM <br />
+            Closed on Mondays
+          </p>
+        </div>
 
+        {/* Google Map Embed */}
+        <div className="max-w-4xl mx-auto mt-8">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.9548618795133!2d104.9166602!3d11.5658706!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3109513d1c436d77%3A0x6cfb8c09d6dbe947!2s179-193%20Preah%20Ang%20Yukanthor%20St%20(St%2019)%2C%20Phnom%20Penh!5e0!3m2!1sen!2skh!4v1724392047469!5m2!1sen!2skh"
+            width="100%"
+            height="400"
+            allowFullScreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+            className="rounded-2xl shadow-md"
+          ></iframe>
+        </div>
+      </div>
       {/* Store Photos */}
       <section className="max-w-5xl mx-auto px-6 pb-10">
         <h2 className="text-xl md:text-2xl font-semibold mb-6 text-center">
